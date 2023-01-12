1b0d5cc00da8ef8801a3e36ee44abfe9e3d39512
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2023-01-12 23:00:20 +0100

    few components

---
+++ src/GeneralComponents/menubar/menubarFirst.jsx
@@ -0,0 +1,121 @@
+import { useNavigate } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { logout } from '../../User/store/user.store';
+
+import './MenuBar.css';
+
+const MenuBar = () => {
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.user.token);
+  const isAdmin = useSelector((state) => state.user.isAdmin);
+  const dispatch = useDispatch();
+
+  const items = [
+    {
+      label: 'Home',
+      icon: 'pi pi-fw pi-home',
+      command: () => navigate('/')
+    },
+    {
+      label: 'User',
+      icon: 'pi pi-fw pi-user',
+      items: [
+        {
+          label: 'Login',
+          icon: 'pi pi-fw pi-sign-in',
+          command: () => navigate('/login'),
+          visible: token === ''
+        },
+        {
+          label: 'Register',
+          icon: 'pi pi-fw pi-user-plus',
+          command: () => navigate('/register'),
+          visible: token === ''
+        },
+        {
+          label: 'Logout',
+          icon: 'pi pi-fw pi-sign-out',
+          visible: token !== '',
+          command: () => {
+            dispatch(logout());
+            navigate('/login');
+          }
+        },
+        {
+          label: 'Profile',
+          icon: 'pi pi-fw pi-user',
+          visible: token !== '',
+          command: () => navigate('/profile')
+        }
+      ]
+    },
+    {
+      label: 'Search',
+      icon: 'pi pi-fw pi-users',
+      items: [
+        {
+          label: 'Filter',
+          icon: 'pi pi-fw pi-filter',
+          items: [
+            {
+              label: 'Print',
+              icon: 'pi pi-fw pi-print'
+            }
+          ]
+        },
+        {
+          icon: 'pi pi-fw pi-bars',
+          label: 'List'
+        }
+      ]
+    },
+    {
+      label: 'Movies',
+      icon: 'pi pi-fw pi-play',
+      visible: token !== '',
+      items: [
+        {
+          label: 'Movies',
+          icon: 'pi pi-fw pi-play',
+          command: () => navigate('/movies')
+        },
+        {
+          label: 'TopMovies',
+          icon: 'pi pi-fw pi-check',
+          command: () => navigate('/topmovies')
+        }
+      ]
+    },
+    {
+      label: 'Series',
+      icon: 'pi pi-fw pi-play',
+      visible: token !== '',
+      items: [
+        {
+          label: 'Series',
+          icon: 'pi pi-fw pi-play',
+          command: () => navigate('/series')
+        },
+        {
+          label: 'TopSeries',
+          icon: 'pi pi-fw pi-check',
+          command: () => navigate('/topseries')
+        }
+      ]
+    },
+    {
+      label: 'Administration',
+      icon: 'pi pi-fw pi-sitemap',
+      visible: isAdmin && token !== '',
+      command: () => navigate('/administration')
+    }
+  ];
+
+  const start = <img src="logo-modified.png" alt="logo" className="mr-2 logo" />;
+
+  return <Menubar className="menubar" model={items} start={start} />;
+};
+
+export default MenuBar;
