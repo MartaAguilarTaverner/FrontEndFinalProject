d2a90287dcab7150242bfbcd96bce06fb6636c4d
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2023-01-27 13:36:05 +0100

    review service(modify after deploy)

--- src/Review/services/review.service.js
+++ src/Review/services/review.service.js
@@ -9,8 +9,44 @@ export default class ReviewService {
     return axios.get(this.url);
   }
 
-  getOneById(token, userid) {
-    return axios.get(`${this.url}/${userid}`, {
+  getOnebyId(token, reviewId) {
+    return axios.get(`${this.url}/${reviewId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  doReview(token, userId, rentedSpaceid) {
+    return axios.post(`${this.url}/${rentedSpaceid}`, {
+      data: {
+        userId,
+        rentedSpaceid
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  modifyReview(token, userId, reviewId) {
+    return axios.put(`${this.url}/${reviewId}`, {
+      data: {
+        userId,
+        reviewId
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  deleteReview(token, userId, reviewId) {
+    return axios.delete(`${this.url}/${reviewId}`, {
+      data: {
+        userId,
+        reviewId
+      },
       headers: {
         Authorization: `Bearer ${token}`
       }
