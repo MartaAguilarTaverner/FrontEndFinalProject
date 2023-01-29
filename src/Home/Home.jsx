347418c6c6e6e035a88f0523d6e6162a8aaf7286
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2023-01-29 10:18:26 +0100

    testing in progress

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import useRoomTypeHook from '../RentedSpace/RoomType/hooks/roomType.hook';
+
 import HomeTable from './components/HomeTable/HomeTable';
 
 export default function Home() {
+  const token = useSelector((state) => state.user.token);
+  const userId = useSelector((state) => state.user.id);
+  const { deleteRoomType } = useRoomTypeHook();
+
+  const genericTestAPI = useCallback(async () => {
+    const roomType = {
+      name: 'Test Create'
+    };
+
+    await deleteRoomType(token, userId, 4);
+  }, []);
+
+  useEffect(() => {
+    if (token !== '') {
+      genericTestAPI();
+    }
+  }, []);
   return <div>THIS IS HOME</div>;
 }
