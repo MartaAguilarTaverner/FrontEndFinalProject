6acbbc5cd9217328fd9c91659edadf7cbdf02bdf
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2023-01-29 23:46:44 +0100

    change admin and home(without tests)

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,26 +1,10 @@
 import React, { useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import useRoomTypeHook from '../RentedSpace/RoomType/hooks/roomType.hook';
+import useRentedSpaceHook from '../RentedSpace/hooks/rentedSpace.hook';
+import useMediaHook from '../RentedSpace/Media/hooks/media.hooks';
 
 import HomeTable from './components/HomeTable/HomeTable';
 
 export default function Home() {
-  const token = useSelector((state) => state.user.token);
-  const userId = useSelector((state) => state.user.id);
-  const { deleteRoomType } = useRoomTypeHook();
-
-  const genericTestAPI = useCallback(async () => {
-    const roomType = {
-      name: 'Test Create'
-    };
-
-    await deleteRoomType(token, userId, 4);
-  }, []);
-
-  useEffect(() => {
-    if (token !== '') {
-      genericTestAPI();
-    }
-  }, []);
   return <div>THIS IS HOME</div>;
 }
--- src/User/Admin/Admin.jsx
+++ src/User/Admin/Admin.jsx
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 
 import AdminMenu from './components/AdminMenu';
-import AdminUsers from './components/AdminUsers';
-import AdminReservations from './components/AdminReservations';
-import AdminRentedSpaces from './components/AdminRentedSpaces';
-import AdminReviews from './components/AdminReviews';
 
 export default function Admin() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <>
-      <div className="admin-header">
-        <AdminMenu activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+      <div>
+        <AdminMenu />
       </div>
-      <div className="admin-content p-4">
-        {activeIndex === 0 && <AdminUsers />}
-        {activeIndex === 1 && <AdminRentedSpaces />}
-        {activeIndex === 2 && <AdminReservations />}
-        {activeIndex === 3 && <AdminReviews />}
+      <div>
+        {/*         {activeIndex === 0 && <AdminUser />}
+        {activeIndex === 1 && <AdminRentedSpace />}
+        {activeIndex === 2 && <AdminReservation />}
+        {activeIndex === 3 &&} */}
       </div>
     </>
   );
